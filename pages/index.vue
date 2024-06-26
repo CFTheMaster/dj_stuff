971299804614d305bf7c484be447bf55912d887a
Emillia <hentaimasterpad@gmail.com>
2024-06-27 00:52:59 +0200

    aaa

---
+++ pages/index.vue
@@ -0,0 +1,8 @@
+<template>
+    <header></header>
+    <main>
+        <h1>Welcome to the homepage</h1>
+        This is an auto-imported component
+    </main>  
+  <MainFooter></MainFooter>
+</template>
